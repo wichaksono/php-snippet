eed2f0d5c2242b3f4de1d42b0595e35180a8a404
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2024-03-19 21:50:01 +0700

    Update check-ip-from-ip-range.php

--- check-ip-from-ip-range.php
+++ check-ip-from-ip-range.php
@@ -1,41 +1,31 @@
 <?php
-ips = '103.117.56.0 - 103.117.56.255|103.117.47.17|103.117.57.6-103.117.57.200|103.117.58.12-|-103.117.52.11';
+$ips = '103.117.56.0 - 103.117.56.255|103.117.47.17|103.117.57.6-103.117.57.200|103.117.58.12-|-103.117.52.11';
 $ips = preg_replace("/\s+/", "", $ips);
 
 $explode = explode('|', $ips);
 
 $ipLists = [];
 foreach ($explode as $item) {
-    if (strpos($item, '-') !== false) {
-        $ipLists[] = explode('-', $item);
-    } else {
-        $ipLists[] = $item;
-    }
+    $ipLists[] = explode('-', $item);
 }
 
 /**
  * Test
  */
-$ipString  = '103.117.52.10';
+$ipString  = '103.117.47.0';
 $currentIp = ip2long($ipString);
 $isBlocked = false;
 foreach ($ipLists as $ipList) {
-    if (! is_array($ipList) && ip2long($ipList) == $currentIp) {
+    $ipLow  = isset($ipList[0]) && $ipList[0] !== '' ? ip2long($ipList[0]) : 0;
+    $ipHigh = isset($ipList[1]) && $ipList[1] !== '' ? ip2long($ipList[1]) : 0;
+
+    if (
+        ($ipLow && $ipHigh && $currentIp >= $ipLow && $currentIp <= $ipHigh)
+        || ($ipLow && $ipHigh == 0 && $ipLow == $currentIp)
+        || ($ipLow == 0 && $ipHigh && $ipHigh == $currentIp)
+    ) {
         $isBlocked = true;
         break;
     }
-
-    if (is_array($ipList)) {
-        $ipLow  = $ipList[0] !== '' ? ip2long($ipList[0]) : 0;
-        $ipHigh = $ipList[1] !== '' ? ip2long($ipList[1]) : 0;
-
-        if (
-            ($ipLow && $ipHigh && $currentIp >= $ipLow && $currentIp <= $ipHigh)
-            || ($ipLow && $ipHigh == 0 && $ipLow == $currentIp)
-            || ($ipLow == 0 && $ipHigh && $ipHigh == $currentIp)
-        ) {
-            $isBlocked = true;
-            break;
-        }
-    }
 }
+
